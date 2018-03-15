4b60413aabdb97cf6fa83b17360fdc17272cd00e
TheUnknow23 <matteo.longato@studenti.unitn.it>
2018-03-15 22:41:07 +0100

    Parking Spot modelled

--- src/main/java/ParkingSpot.java
+++ src/main/java/ParkingSpot.java
@@ -1,3 +1,21 @@
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+
 public class ParkingSpot {
     int id;
+    boolean occupied = false;
+    float lat;
+    float lon;
+    Marker marker;
+
+    ParkingSpot(float lat, float lon, boolean occupied){
+        this.lat = lat;
+        this.lon = lon;
+        this.occupied = occupied;
+        MarkerOptions markerOptions = new MarkerOptions();
+        LatLong pos = new LatLong(lat, lon);
+        markerOptions.position(pos);
+        marker = new Marker(markerOptions);
+    }
 }
