9dbfabe394082428d8bd515cadd1e0e367f2a3dc
TheUnknow23 <matteo.longato@studenti.unitn.it>
2018-03-16 00:43:22 +0100

    Database fetch implemented

--- src/main/java/LatLongFXMLController.java
+++ src/main/java/LatLongFXMLController.java
@@ -1,3 +1,4 @@
+import DB.DBConnector;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.event.GMapMouseEvent;
@@ -5,8 +6,11 @@ import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.*;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.text.DecimalFormat;
-import java.util.ResourceBundle;
+import java.util.*;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -33,6 +37,32 @@ public class LatLongFXMLController implements Initializable {
     }
 
     protected void configureMap() {
+        ArrayList<ParkingSpot> Spots = new ArrayList<ParkingSpot>();
+        DBConnector DB = new DBConnector();
+        Connection con = DB.connect("localhost", 3306, "test", "admin", "admin");
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Spots.clear();
+                ResultSet results = DB.executeQuery("select * from filari", con);
+                try{
+                    while (results.next()){
+                        int filareId = results.getInt(1);
+                        int id = results.getInt(2);
+                        boolean occupied = results.getBoolean(1);
+                        Date arrivalTime = results.getTime(1);
+                        String targa = results.getString(1);
+                        float lat = results.getFloat(1);
+                        float lon = results.getFloat(2);
+                        ParkingSpot parkingSpot = new ParkingSpot(filareId, id, arrivalTime, targa, lat, lon, occupied);
+                        Spots.add(parkingSpot);
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, 10*1000);
         MapOptions mapOptions = new MapOptions();
 
         mapOptions.center(new LatLong(46.071358, 11.124731))  //Trento centro
