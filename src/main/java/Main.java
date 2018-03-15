c728e18dfeba560931eb7945458b55b6f9fe0047
giulio <giulioinfo@gmail.com>
2018-03-15 21:17:23 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("yolo");
+        System.out.println("secondo push di prova per puro esperimento");
     }
 }
