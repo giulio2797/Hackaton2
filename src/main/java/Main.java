63dae701e6d16133dcc94e13f6af8411700d6d3a
giulio <giulioinfo@gmail.com>
2018-03-15 23:19:12 +0100

    FUNZIONA!!!!!!!!!!!!!!!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,14 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application
 {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/lynden/example/latlong/Scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Scene.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
