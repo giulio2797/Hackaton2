8cecd591d603b72d4fa7ada7c3372883f7974ac0
giulio <giulioinfo@gmail.com>
2018-03-15 21:38:31 +0100

    google api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("yolo");
-        System.out.println("secondo push di prova per puro esperimento");
+        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyBqIHo3LmmfthV8q0tpibDuNy7DfBlgUE0").build();
+        GeocodingResult[] results =  GeocodingApi.geocode(context,"1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(results[0].addressComponents));
     }
 }
